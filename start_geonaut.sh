37482c266d010e91e33b89a563e46b1b7116a4e6
Nico Jones <nico@speal.ai>
2024-07-14 20:03:50 +0200

    add comments to build script

--- start_geonaut.sh
+++ start_geonaut.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+echo "cd to folder"
 cd /var/www/geonaut/nextjs
+
+echo "running npm install..."
 npm i
+
+echo "generating production build..."
 npm run build:prod
+
+echo "starting server"
 npm run start
