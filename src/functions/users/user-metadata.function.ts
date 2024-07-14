b043e38cf9639921c13868d2430fde5d7fc38125
Nico Jones <nico@speal.ai>
2024-07-14 21:23:33 +0200

    fix og-image issues

--- src/functions/users/user-metadata.function.ts
+++ src/functions/users/user-metadata.function.ts
@@ -14,14 +14,14 @@ export const userMetadata = (user: IUserData): Metadata => ({
     siteName: "geonaut",
     images: [{
       // TODO -- will not work for users with a default picture
-      url: `${process.env.NEXT_PUBLIC_RESOURCE_URL as string}/${user.avatar}`,
+      url: user.avatar,
     }],
   },
   twitter: {
     title: user.name + " - geonaut",
     description: user.profile,
     images: [{
-      url: `${process.env.NEXT_PUBLIC_API_URL as string}${user.avatar}`,
+      url: user.avatar,
     }],
   },
 });
