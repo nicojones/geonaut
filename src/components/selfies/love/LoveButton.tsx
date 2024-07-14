a740bda2ec8498e89cd86b4b5aeccb3193d311a0
Nico Jones <nico@speal.ai>
2024-07-14 21:37:47 +0200

    fix love count on webkit browsers

--- src/components/selfies/love/LoveButton.tsx
+++ src/components/selfies/love/LoveButton.tsx
@@ -7,8 +7,8 @@ interface LoveButtonProps {
 export const LoveButton = ({ count }: LoveButtonProps): JSX.Element => {
   return (
     <>
-      <HeartIcon className="size-10 text-red-500" />
       <span className="absolute top-5 left-5 text-white translate-x-[-50%] translate-y-[-50%] text-sm">{count}</span>
+      <HeartIcon className="size-10 text-red-500" />
     </>
   );
 };
