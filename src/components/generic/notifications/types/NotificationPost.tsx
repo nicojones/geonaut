11a5ee8c70c6e8ea4a1c518959b36552cb46c5fe
Nico Jones <nico@speal.ai>
2024-07-15 11:16:34 +0200

    Post notification type

--- src/components/generic/notifications/types/NotificationPost.tsx
+++ src/components/generic/notifications/types/NotificationPost.tsx
@@ -1,13 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { selfieCpImage } from "@/functions";
 import { INotification } from "@/types";
 
 interface NotificationPostProps {
   notification: INotification;
 }
 
-export const NotificationPost = ({ notification }: NotificationPostProps): JSX.Element => {
+export const NotificationPost = ({ notification: n }: NotificationPostProps): JSX.Element => {
   return (
-    <>
-      post {JSON.stringify(notification)}
-    </>
+    <div className="fric space-x-2">
+      <Image width={100} height={150} src={selfieCpImage({ hash: n.hash })} alt={n.title} />
+      <div className="flex flex-col justify-around">
+        <span className="text-sm">
+          <Link
+            href={`/u/${n.username}`}
+            className="font-bold cursor-pointer hover:underline"
+          >{n.name}
+          </Link>
+          &nbsp;
+          has uploaded a new post
+        </span>
+        <Link
+          href={`/s/${n.active_hash}`}
+          className="font-bold cursor-pointer hover:underline"
+        >{n.title}
+        </Link>
+      </div>
+    </div>
   );
 };
