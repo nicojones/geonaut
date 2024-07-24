a53ab4c55b240011c042825f02b0224c4a09c466
Nico Jones <nico@speal.ai>
2024-07-24 11:32:34 +0200

    Fix sign-in sign-up token handling

--- src/app/auth/sign-in/SignInForm.tsx
+++ src/app/auth/sign-in/SignInForm.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import Cookies from "js-cookie";
 import { useFormState } from "react-dom";
 
-import { ILoginFormFields } from "@/types";
+import { useJwtTokenContext } from "@/context";
+import { ILoginFormFields, IStorageKey } from "@/types";
 
 import { signInSubmit } from "./sign-in-submit.function";
 import { SignInFormFields } from "./SignInFormFields";
@@ -10,7 +12,19 @@ import { SignInFormFields } from "./SignInFormFields";
 const INITIAL_STATE: Partial<ILoginFormFields> = {};
 
 export const SignInForm = (): JSX.Element => {
-  const [errors, formAction] = useFormState<Partial<ILoginFormFields>, any>(signInSubmit, INITIAL_STATE);
+  const { setJwt } = useJwtTokenContext();
+
+  const handleFormSubmit = (_prevState: Partial<ILoginFormFields>, data: FormData): Promise<any> => {
+    return signInSubmit(_prevState, data).then((r) => {
+      const tokenCookie: string | undefined = Cookies.get("token" satisfies IStorageKey);
+      if (tokenCookie) {
+        setJwt(tokenCookie);
+      }
+      return r;
+    });
+  };
+
+  const [errors, formAction] = useFormState<Partial<ILoginFormFields>, any>(handleFormSubmit, INITIAL_STATE);
 
   return (
     <>
--- src/app/auth/sign-up/SignUpForm.tsx
+++ src/app/auth/sign-up/SignUpForm.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import Cookies from "js-cookie";
 import { useFormState } from "react-dom";
 
-import { ISignupFormFields } from "@/types";
+import { useJwtTokenContext } from "@/context";
+import { ISignupFormFields, IStorageKey } from "@/types";
 
 import { signUpSubmit } from "./sign-up-submit.function";
 import { SignUpFormFields } from "./SignUpFormFields";
@@ -10,11 +12,22 @@ import { SignUpFormFields } from "./SignUpFormFields";
 const INITIAL_STATE: Partial<ISignupFormFields> = {};
 
 export const SignUpForm = (): JSX.Element => {
-  const [errors, formAction] = useFormState<Partial<ISignupFormFields>, any>(signUpSubmit, INITIAL_STATE);
+  const { setJwt } = useJwtTokenContext();
+
+  const handleFormSubmit = (_prevState: Partial<ISignupFormFields>, data: FormData): Promise<any> => {
+    return signUpSubmit(_prevState, data).then((r) => {
+      const tokenCookie: string | undefined = Cookies.get("token" satisfies IStorageKey);
+      if (tokenCookie) {
+        setJwt(tokenCookie);
+      }
+      return r;
+    });
+  };
+
+  const [errors, formAction] = useFormState<Partial<ISignupFormFields>, any>(handleFormSubmit, INITIAL_STATE);
 
   return (
     <>
-      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
       <form action={formAction}>
         <SignUpFormFields errors={errors} />
       </form>
