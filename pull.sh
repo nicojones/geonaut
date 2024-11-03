c0dd869104c5cfc88bbbc56f28639e99bbe25cd5
Nico Jones <nico@speal.ai>
2024-11-03 13:57:55 +0100

    Fix build.sh script

--- pull.sh
+++ pull.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+echo "Stopping geonaut..."
 systemctl stop geonaut
 
+echo "Checking out the new build (orphan branch)"
 git checkout main
 git branch -D next-build
 git checkout --orphan next-build
 git pull origin next-build
 
+echo "Cleaning up old files..."
 rm -rf node_modules
 rm -rf __MACOSX
 
-unzip -d . ../node_modules.zip | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
+echo "Unzipping node_modules.zip ..."
+unzip -d . ../node_modules.zip | awk 'BEGIN {ORS=""} {if(NR%1000==0)print "."}'
 rm -rf __MACOSX
 
+echo "Starting geonaut..."
 systemctl start geonaut
+
+echo "🎉 Started! https://travel.kupfer.es"
