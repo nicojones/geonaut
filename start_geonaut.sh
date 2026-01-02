d2a81c11646d69fc3906195740f368c7bd8e99ec
Nico Jones <nico@speal.ai>
2026-01-02 16:19:37 +0100

    fix zip symlinks

--- start_geonaut.sh
+++ start_geonaut.sh
@@ -1,36 +1,14 @@
 #!/bin/bash
-
-# Function to stop the script
-stop_script() {
-    echo "Stopping the script..."
-    PID=$(ps aux | grep 'next-server' | awk '{print $2}')
-    if [ -z "$PID" ]; then
-        echo "No running script found."
-    else
-        kill -9 $PID
-        echo "Script stopped successfully."
-    fi
-}
-
-# Check for --stop argument
-if [ "$1" == "--stop" ]; then
-    stop_script
-    exit 0
-
-elif [ "$1" == "--restart" ]; then
-    stop_script
-    # do not exit
-fi
-
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "cd to folder..."
-cd /var/www/geonaut/nextjs
+# Navigate to the app directory
+cd /var/www/geonaut/nextjs-2
 
+# Load NVM and Node
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # Load nvm
-nvm use 20 # Use Node 20
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 20
 
-echo "starting server"
-npm run start
+# Run Next.js
+echo "Starting Geonaut on Port 3003..."
+exec npm run start -- -p 3003
